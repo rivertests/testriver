c0ca6d7f631ad983b8b5ccc6a007ad65d5fe9244
semtetog <brennodmoraes@gmail.com>
2025-08-22 10:42:35 -0300

    Commit inicial do projeto

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -32,9 +32,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WKNavigationDelegate { //
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
         // Código Adicionado para configurar o delegate
-        if let bridgeVC = self.window?.rootViewController as? CAPBridgeViewController {
-            bridgeVC.getWebView()?.navigationDelegate = self
-        }
+        // ESTA É A LINHA CORRETA
+if let bridgeVC = self.window?.rootViewController as? CAPBridgeViewController {
+    bridgeVC.bridge?.webView?.navigationDelegate = self
+}
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
